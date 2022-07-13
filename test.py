0c8d6b199d7930ab0ada2c5618188cd7cf18c588
Gintautas Minelga <morewind@gmail.com>
2022-07-13 15:47:55 +0300

    initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+import sqlite3 as sl
+con=sl.connect('test.db')
+cursor = con.cursor()
+
+
+cursor.execute("CREATE TABLE db (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, speed FLOAT, gust FLOAT, direction FLOAT, pressure FLOAT, temperature FLOAT, timestamp STRING)")
+               
+speed, gust, direction, pressure, temperature, timestamp = 5.5, 5.6, 33, 1020, 23, "kovas"
+print(speed, gust, direction, pressure, temperature, timestamp)
+
+cursor.executemany("insert into db values (?,?,?,?,?,?)", [speed, gust, direction, pressure, temperature, timestamp])
